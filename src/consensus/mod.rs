c3fd07943e264a0a3165a240a170638919bd484e
johnnas12 <yohanesmesfin3@gmail.com>
2026-04-14 12:26:12 +0300

    Wire up consensus module in crate root
    Add consensus module with re-exports of ForkChoice, fork_choice,
    collect_validator_tips, and is_cordial.

---
+++ src/consensus/mod.rs
@@ -0,0 +1,3 @@
+pub mod fork_choice;
+
+pub use fork_choice::{ForkChoice, fork_choice, collect_validator_tips, is_cordial};
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod block;
 pub mod blocklace;
 pub mod crypto;
 pub mod network;
+pub mod consensus;
 
 pub use types::{NodeId, BlockContent, BlockIdentity};
 pub use block::Block;
