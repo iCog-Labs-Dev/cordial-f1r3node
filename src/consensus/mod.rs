9e4f2852a648f19c3f92e93c557e445989edc269
johnnas12 <yohanesmesfin3@gmail.com>
2026-04-14 12:51:39 +0300

    Export finality module from consensus
    Re-export FinalityStatus, check_finality, find_last_finalized,
    and can_be_finalized.

--- src/consensus/mod.rs
+++ src/consensus/mod.rs
@@ -1,3 +1,5 @@
 pub mod fork_choice;
+pub mod finality;
 
 pub use fork_choice::{ForkChoice, fork_choice, collect_validator_tips, is_cordial};
+pub use finality::{FinalityStatus, check_finality, find_last_finalized, can_be_finalized};
